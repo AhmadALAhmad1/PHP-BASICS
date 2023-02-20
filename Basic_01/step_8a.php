1a71fe52dd1de0ef8ec7150ca1c53e39c4499cbe
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-02-21 01:05:23 +0200

    step_8a

---
+++ Basic_01/step_8a.php
@@ -0,0 +1,6 @@
+<?php
+$grocery = array("Eggs", "Milk", "Cheese","Water Pack", "Tissues", "Watermelon");
+
+echo "Hello Sir, do you have " .$grocery[0].",".$grocery[1]. ", and ".$grocery[2]."?";
+echo "\nAlso if you sell fruits can I find a ". $grocery[5]. "?";
+?>
