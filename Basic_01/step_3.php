6100ee9c3786322fdde1cb9c55ae2aaf088c7591
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-02-20 11:05:41 +0200

    step3

---
+++ Basic_01/step_3.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Ahmad";
+$email = "ahmadd.l.ahmadd1998@gmail.com";
+$height = 100;
+$width = 200;
+
+echo "My name is ",$name," and my email is ",$email,"\n";
+
+echo "The area value is ",$height * $width,"\n";
+
+
+
+
+?>
