e080196e4329812d72e2329db100edd4a284877a
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-02-20 11:47:31 +0200

    step4

---
+++ Basic_01/step_4.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Ahmad";
+$position = "Backend Developer";
+$github_url = "https://github.com/AhmadLAhmad";
+
+echo "Hello, I'am " .$name." I'm a ".$position.", please check my github link ".$github_url."\n" ;
+echo "Hello, I'am " ,$name," I'm a ",$position,", please check my github link ",$github_url,"\n" ;
+echo 'Hello, Iam ', $name, 'Iam a ' ,$position, ' please check my github link ',$github_url,"\n" ;
+
+?>
