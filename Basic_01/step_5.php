b98463094f6b7baff69b0654bceedc77930dece2
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2023-02-20 12:23:01 +0200

    step5

---
+++ Basic_01/step_5.php
@@ -0,0 +1,12 @@
+<?php
+$course_name = "Nodejs";
+$enrolled_students = 10;
+$price = 20;
+$on_discount = "yes/no";
+
+echo "\nCourse title: " .$course_name;
+echo "\nEnrolled Students:" .$enrolled_students;
+echo"\n Course price: " .$price. "$ USD";
+echo" \nCourse on discount:" .$on_discount ."\n";
+
+?>
